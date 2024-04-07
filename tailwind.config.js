a413a2aece495f75aceced8342a05376c2b23186
Rey <reycrisbf@gmail.com>
2024-04-07 17:30:02 -0400

    updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,22 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             colors: {
+                whatsapp: {
+                    light: {
+                        primary: '#25D366',
+                        secondary: '#075E54',
+                        lightGray: '#E5E5E5',
+                        darkGray: '#D8D8D8',
+                        white: '#FFFFFF',
+                    },
+                    dark: {
+                        primary: '#25D366',
+                        secondary: '#075E54',
+                        lightGray: '#1C1C1C',
+                        darkGray: '#0F0F0F',
+                        black: '#000000',
+                    },
+                },
                 'forest-green': '#008069',
                 'apple-green': '#25d366',
                 'light-gray': '#fcfefd',
